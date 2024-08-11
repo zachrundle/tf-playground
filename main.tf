a4d6d7c2efaf1e487cbf0d8c1f837ffadbf8969e
cybershady <zjrundle@gmail.com>
2024-08-11 15:51:59 -0600

    more action changes'

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "test-bucket-42353242213123"
   tags = {
     Name        = "MyS3Bucket"
-    Environment = "Dev"
+    Environment =                       "Dev"
   }
 }
