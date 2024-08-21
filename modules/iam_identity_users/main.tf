e4e742b0cd9debd730d056e47375941f490e287d
cybershady <zjrundle@gmail.com>
2024-08-20 20:41:35 -0600

    add back in for_each

--- modules/iam_identity_users/main.tf
+++ modules/iam_identity_users/main.tf
@@ -1,19 +1,20 @@
 data "aws_ssoadmin_instances" "this" {}
 
 
-
 resource "aws_identitystore_user" "this" {
+  for_each = var.users
+
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
-  display_name = format("%s %s", users.first_name,  users.last_name)
-  user_name    = format("%s%s", substr(lower( users.first_name), 0, 1), lower( users.last_name))
+  display_name = format("%s %s", each.value.first_name, each.value.last_name)
+  user_name    = format("%s%s", substr(lower(each.value.first_name), 0, 1), lower(each.value.last_name))
 
   name {
-    given_name  =  users.first_name
-    family_name =  users.last_name
+    given_name  = each.value.first_name
+    family_name = each.value.last_name
   }
 
   emails {
-    value = join("@", [format("%s.%s", lower( users.first_name), lower( users.last_name)), var.email_domain])
+    value = join("@", [format("%s.%s", lower(each.value.first_name), lower(each.value.last_name)), var.email_domain])
   }
 }
