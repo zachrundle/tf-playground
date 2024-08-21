9b07c1e0d41ee9ad036b62f3ad0a859d7f3eede2
cybershady <zjrundle@gmail.com>
2024-08-20 20:42:56 -0600

    add diff var names

--- users.tf
+++ users.tf
@@ -1,11 +1,11 @@
 module "users" {
   source           = "./modules/iam_identity_users"
   users = {
-    "user" = {
+    "Zach Rundle" = {
       first_name = "Zach"
       last_name   = "Rundle"
     },
-    "user" = {
+    "Maverick Dog" = {
       first_name = "Maverick"
       last_name   = "Dog"
     },
