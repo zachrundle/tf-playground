77f5ca43d9f9c85e29de2f3ae50e8e2be2796150
cybershady <zjrundle@gmail.com>
2024-08-20 23:12:07 -0600

    fix syntax

--- modules/iam_identity_users/main.tf
+++ modules/iam_identity_users/main.tf
@@ -1,12 +1,9 @@
-# Fetching SSO Instance
-data "aws_ssoadmin_instances" "this" {}
-
 # Create SSO Groups
 resource "aws_identitystore_group" "this" {
-  for_each         = toset(flatten([for user in values(var.users) : user.groups]))
+  for_each         = { for group_name in toset(flatten([for user in values(var.users) : user.groups])) : group_name => group_name }
 
-  display_name      = each.key
-  description       = format("SSO group for %s", each.key)
+  display_name      = each.value
+  description       = format("SSO group for %s", each.value)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 }
 
@@ -31,9 +28,9 @@ resource "aws_identitystore_user" "this" {
 
 # Assign Users to Groups
 resource "aws_identitystore_group_membership" "this" {
-  for_each = { for user_key, user in var.users : user_key => user }
+  for_each = { for user_key, user in var.users : user_key => { for group_name in user.groups : group_name => aws_identitystore_group.this[group_name].id } }
 
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  group_id          = aws_identitystore_group.this[each.value.groups].id
+  group_id          = each.value[group_name]
   member_id         = aws_identitystore_user.this[each.key].id
 }
