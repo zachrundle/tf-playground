efe89219c020731012c79dc52d16f416a01f4e75
cybershady <zjrundle@gmail.com>
2024-08-25 00:50:46 -0600

    remove string brackets

--- users.tf
+++ users.tf
@@ -6,12 +6,12 @@ module "users" {
     "Zach Rundle" = {
       first_name = "Zach"
       last_name  = "Rundle"
-      groups     = ["administrators"]
+      groups     = "administrators"
     },
     "Maverick Dog" = {
       first_name = "Maverick"
       last_name  = "Dog"
-      groups     = ["developers"]
+      groups     = "developers"
     },
   }
 }
