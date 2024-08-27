c5e6cb5a68e22de5fe65151e3e9dbbe49a99eb85
cybershady <zjrundle@gmail.com>
2024-08-26 21:54:43 -0600

    combine iam identity modules

--- modules/iam_identity_center/main.tf
+++ modules/iam_identity_center/main.tf
@@ -64,3 +64,49 @@ resource "aws_ssoadmin_customer_managed_policy_attachment" "this" {
     path = coalesce(each.value.policy_path, "/")
   }
 }
+
+# Fetching SSO Instance
+data "aws_ssoadmin_instances" "this" {}
+
+# Create SSO Groups
+resource "aws_identitystore_group" "this" {
+  for_each          = { for group_name in var.groups : group_name => group_name }
+  display_name      = each.value
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+}
+
+# Create SSO Users
+resource "aws_identitystore_user" "this" {
+  for_each = var.users
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+  display_name = format("%s %s", each.value.first_name, each.value.last_name)
+  user_name    = format("%s%s", substr(lower(each.value.first_name), 0, 1), lower(each.value.last_name))
+
+  name {
+    given_name  = each.value.first_name
+    family_name = each.value.last_name
+  }
+
+  emails {
+    value = join("@", [format("%s.%s", lower(each.value.first_name), lower(each.value.last_name)), var.email_domain])
+  }
+}
+
+# Assign Users to Groups
+resource "aws_identitystore_group_membership" "this" {
+  for_each =  var.users
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+  group_id          = aws_identitystore_group.this[each.value.groups].group_id
+  member_id         = aws_identitystore_user.this[each.key].user_id
+}
+
+resource "aws_ssoadmin_account_assignment" "this" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  permission_set_arn = data.aws_ssoadmin_permission_set.example.arn
+
+  principal_id   = data.aws_identitystore_group.this.group_id
+  principal_type = "GROUP"
+
+  target_id   = "123456789012"
+  target_type = "AWS_ACCOUNT"
+}
